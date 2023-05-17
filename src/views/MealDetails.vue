2dc47948f05505653e528d8d5df50ce7541f0efb
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-05-17 12:02:08 +0100

    Other icon for blog
    Co-Authored-By: freeCoding <132020771+freeCoding88@users.noreply.github.com>

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,28 +1,49 @@
 <template>
   <div class="max-w-full mx-auto p-8">
     <MealItem :meal="meal" />
-    <div class="icons-share">
-      <h4 class="my-4 font-semibold text-indigo-800 text-lg">Share This:</h4>
-      <s-email :share-options="shareOptions">
-      <svg
-        fill="none"
-        stroke="currentColor"
-        stroke-width="1.5"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
-        ></path>
-        </svg>
-      </s-email>
+    <div class="icons-share bg-white p-5 rounded-lg border">
+      <h4 class="mb-4 font-semibold text-indigo-600 text-xl">Share This:</h4>
+      <div class="flex items-center">
+        <s-email :share-options="shareOptions">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+            ></path>
+          </svg>
+        </s-email>
+        <s-blogger
+          :window-features="windowFeatures"
+          :share-options="shareOptionsBlog"
+          :use-native-behavior="useNativeBehavior"
+        >
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            ></path>
+          </svg>
+        </s-blogger>
+      </div>
     </div>
     <hr class="my-5" />
     <MealItemTabs :meal="meal" />
-   
   </div>
 </template>
 
@@ -33,7 +54,7 @@ import { useRoute } from "vue-router";
 import axiosClient from "../axiosClient";
 import MealItem from "../components/MealItem.vue";
 import MealItemTabs from "../components/MealItemTabs.vue";
-import { SEmail } from "vue-socials";
+import { SEmail, SBlogger } from "vue-socials";
 
 const meal = ref({});
 const route = useRoute();
@@ -44,6 +65,13 @@ const shareOptions = {
   subject: "Subject",
   body: "Hello\nWorld",
 };
+const windowFeatures = {};
+const shareOptionsBlog = {
+  url: "https://github.com/",
+  title: "Title",
+  text: "Text",
+};
+const useNativeBehavior = false;
 
 onMounted(() => {
   const mealId = route.params.id;
